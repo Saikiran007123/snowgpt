36b7cda0085db712af856ac14cf72d0cf7268c51
Saikiran007123 <97353091+Saikiran007123@users.noreply.github.com>
2023-09-07 02:21:10 -0700

    Update config.py

--- SnowGPT_ChatBot/config.py
+++ SnowGPT_ChatBot/config.py
@@ -1,11 +1,3 @@
-# snowflake_user = "VINAYKUMAR"
-# snowflake_password = "Vinay@123"
-# snowflake_account = "fysuwth-ffb68347"
-# snowflake_warehouse = "COMPUTE_WH"
-# snowflake_database = "SNOWGPT_DB"
-# snowflake_schema = "snowgpt_schema"
-# stage_name = "snowgpt_stage"
-
 snowflake_user = "SNOWGPT"
 snowflake_password = "SnowGPT@202308"
 snowflake_account = "anblicksorg_aws.us-east-1"
